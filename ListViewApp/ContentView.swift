6f51a202847628e237c03f74ca7e2cb05d31f587
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-09-06 15:58:06 +0900

    Initial Commit

---
+++ ListViewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewApp
+//
+//  Created by 金野利哉 on 2020/09/06.
+//  Copyright © 2020 金野利哉. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
