2ed5f94727af91ce26701cfc8337ee80353ed36e
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-09-10 12:08:16 +0900

    FirstCommit

--- ListViewApp/ContentView.swift
+++ ListViewApp/ContentView.swift
@@ -7,11 +7,52 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    @ObservedObject var nList = namesList()
+    
+    @ObservedObject var tList = TaskList()
+    
+    @State var newTask : String = ""
+    
+    var addTaskBar : some View{
+        HStack{
+            TextField("追加してください",text:self.$newTask)
+            Button(action: self.addNewTask, label:{Text("add")})
+        }
     }
+    
+    func addNewTask(){
+        tList.tasks.append(Task(id: String(tList.tasks.count + 1), taskItem: newTask))
+        self.newTask = ""
+    }
+    
+    var body: some View {
+        NavigationView {VStack {
+            addTaskBar.padding()
+            List {
+                ForEach(self.tList.tasks){
+                    Index in Text(Index.taskItem)
+                }
+//   デリート機能
+                .onDelete(perform: rowRemove)
+               
+            }
+        .navigationBarTitle("Taskリスト")
+            /// Editボタンを追加
+                       .navigationBarItems(trailing: EditButton())
+            }
+            
+
+            
+    }
+}
+    
+    /// 行削除処理
+func rowRemove(offsets: IndexSet) {
+    tList.tasks.remove(atOffsets: offsets)
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +60,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+}
+
---
+++ ListViewApp/TaskFile.swift
@@ -0,0 +1,31 @@
+//
+//  TaskFile.swift
+//  ListViewApp
+//
+//  Created by 金野利哉 on 2020/09/06.
+//  Copyright © 2020 金野利哉. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+struct Task: Identifiable {
+    var id = String()
+    var taskItem = String ()
+    
+}
+
+class TaskList: ObservableObject {
+    @Published var tasks = [Task]()
+    
+}
+
+
+
+
+
+class namesList: ObservableObject {
+    
+    @Published var names:[String] = ["米倉","川田","磯部","渋谷","加藤",]
+}
